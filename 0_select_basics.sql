2a226864bcf99c366872e8b195d6f264963a2ba0
Murodjoni Sharifzoda <murodjoni@aol.com>
2024-03-18 18:36:17 -0600

    Show Germany population

---
+++ 0_select_basics.sql
@@ -0,0 +1,5 @@
+-- 1
+-- The example uses a WHERE clause to show the population of 'France'. Note that strings should be in 'single quotes';
+
+SELECT population FROM world
+  WHERE name = 'Germany'
