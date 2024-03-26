778e1c1b7bbb67e1a8144d11299031e3ac19d260
Murodjoni Sharifzoda <murodjoni@aol.com>
2024-03-26 10:34:16 -0600

    Started working on SELECT inside SELECT

---
+++ 4_SELECT_within_SELECT/4_SELECT_within_SELECT.sql
@@ -0,0 +1,109 @@
+--1
+--List each country name where the population is larger than that of 'Russia'.
+--world(name, continent, area, population, gdp)
+SELECT
+    name
+FROM
+    world
+WHERE
+    population < (
+        SELECT
+            population
+        FROM
+            world
+        WHERE
+            name = 'Russia'
+    )
+    --2
+    --Show the countries in Europe with a 
+    --per capita GDP greater than 'United Kingdom'.
+SELECT
+    name
+FROM
+    world
+WHERE
+    continent = "Europe"
+    AND gdp / population > (
+        SELECT
+            gdp / population
+        FROM
+            world
+        WHERE
+            name = "United Kingdom"
+    )
+    --3
+    --List the name and continent of countries in the continents 
+    --containing either Argentina or Australia. Order by name of the country.
+FROM
+    world
+WHERE
+    continent = "South America"
+    OR continent = "Insular Oceania"
+ORDER BY
+    name
+    --4 
+    --Which country has a population that is more than United Kingdom 
+    --but less than Germany? Show the name and the population.
+SELECT
+    name,
+    population
+FROM
+    world
+WHERE
+    population > (
+        SELECT
+            population
+        FROM
+            world
+        WHERE
+            name = "United Kingdom"
+    )
+    AND population < (
+        SELECT
+            population
+        FROM
+            world
+        WHERE
+            name = "Germany"
+    )
+    --5
+    --Germany (population 80 million) has the largest population of the countries in Europe. 
+    --Austria (population 8.5 million) has 11% of the population of Germany.
+    --Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+SELECT
+    name,
+    CONCAT (
+        ROUND(
+            100 * population / (
+                SELECT
+                    population
+                FROM
+                    world
+                WHERE
+                    name = 'Germany'
+            )
+        ),
+        '%'
+    ) AS Percentage_of_Germany
+FROM
+    world
+WHERE
+    continent = 'Europe'
+    --6
+    --Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+SELECT
+    name
+FROM
+    world
+WHERE
+    continent <> 'Europe'
+    AND gdp >= ALL (
+        SELECT
+            gdp
+        FROM
+            world
+        WHERE
+            continent = 'Europe'
+            AND gdp > 0
+    )
+    --7
